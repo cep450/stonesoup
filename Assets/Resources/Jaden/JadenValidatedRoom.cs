c3028221b632065ddf02adb99975a23e139f1d15
jaden <cep450@nyu.edu>
2022-03-10 14:07:33 -0500

    room generation and validation

--- Assets/Resources/Jaden/JadenValidatedRoom.cs
+++ Assets/Resources/Jaden/JadenValidatedRoom.cs
@@ -79,6 +79,7 @@ public class JadenValidatedRoom : Room
         return indexGrid;
     }
 
+//breadth first search 
     bool DoesPathExist(int[,] indexGrid, Vector2Int startPoint, Vector2Int endPoint) {
         List<Vector2Int> openSet = new List<Vector2Int>();
         List<Vector2Int> closedSet = new List<Vector2Int>();
@@ -116,7 +117,6 @@ public class JadenValidatedRoom : Room
         }
         return false; //no path 
     }
-
     void tryAdd(Vector2Int neighbor, List<Vector2Int> openSet, List<Vector2Int> closedSet, int[,] indexGrid) {
         if(openSet.Contains(neighbor) == false && closedSet.Contains(neighbor) == false) {
                 if(IsPointNavigable(indexGrid, neighbor))
@@ -143,6 +143,98 @@ public class JadenValidatedRoom : Room
         (point.y < 0 || point.y >= LevelGenerator.ROOM_HEIGHT))
             return false;
         return true;
+    }
+
+
+
+
+
+    class SearchVertex {
+        public Vector2Int point;
+        public SearchVertex parent;
+    }
+
+    //depth first search  and  returns the path it found 
+    //for depth first, it's the same agorithm but the list of nodes is a stack instead of a queue. 
+    //returns the path it found. 
+    //"get depth first search path" 
+    List<Vector2Int> GetDFSPath(int[,] indexGrid, Vector2Int startPoint, Vector2Int endPoint) {
+        List<SearchVertex> openSet = new List<SearchVertex>();
+        List<SearchVertex> closedSet = new List<SearchVertex>();
+        
+        if(IsPointNavigable(indexGrid, startPoint)) {
+            SearchVertex startVertex = new SearchVertex();
+            startVertex.point = startPoint;
+            startVertex.parent = null;
+            openSet.Add(startVertex);
+            //if not nav, skips entire loop 
+        }
+
+        while(openSet.Count > 0) {
+            
+            //literally the only change to make it depth first is the index used in this chunk of code. 
+            int index = openSet.Count - 1;
+            SearchVertex currentVertex = openSet[index];
+            openSet.RemoveAt(index);
+
+            if(currentVertex.point == endPoint) {
+                List<Vector2Int> path = new List<Vector2Int>();
+                while(currentVertex != null) {
+                    path.Add(currentVertex.point);
+                }
+                path.Reverse();
+                return path; 
+            }
+            
+            
+            //make sure this isn't already in 
+            //alternate implementation: other data structure, flag as visited- higher performance
+            //cause, Contains is high overhead 
+            //this implementation is clarity > performance 
+            Vector2Int upNeighbor = new Vector2Int(currentVertex.point.x, currentVertex.point.y + 1);
+            tryAdd(upNeighbor, openSet, closedSet, indexGrid, currentVertex);
+            Vector2Int downNeighbor = new Vector2Int(currentVertex.point.x, currentVertex.point.y - 1);
+            tryAdd(downNeighbor, openSet, closedSet, indexGrid, currentVertex);
+            Vector2Int leftNeighbor = new Vector2Int(currentVertex.point.x - 1, currentVertex.point.y);
+            tryAdd(leftNeighbor, openSet, closedSet, indexGrid, currentVertex);
+            Vector2Int rightNeighbor = new Vector2Int(currentVertex.point.x + 1, currentVertex.point.y);
+            tryAdd(rightNeighbor, openSet, closedSet, indexGrid, currentVertex);
+
+            closedSet.Add(currentVertex);
+
+        }
+
+        return new List<Vector2Int>(); //no path. could return null or an empty list 
+            //returning an empty list means anything can work with the output w/o checking null 
+    }
+
+    bool DoesListContainPoint(List<SearchVertex> list, Vector2Int point) {
+        //lambda way: 
+        //openSet.Exists(x=>x.point==upNeighbor)
+        //"for each value of x, treat it as x.point, do this comparison" 
+        //so comparing against child data instead of data itself 
+        //getting at data inside of class 
+
+        //conventional way 
+        foreach(SearchVertex searchVertex in list) {
+            if(searchVertex.point == point) {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+
+    void tryAdd(Vector2Int neighbor, List<SearchVertex> openSet, List<SearchVertex> closedSet, int[,] indexGrid, SearchVertex currentVertex) {
+        if(DoesListContainPoint(openSet, neighbor) == false && DoesListContainPoint(closedSet, neighbor) == false) {
+            if(IsPointNavigable(indexGrid, neighbor)) {
+                SearchVertex neighborVertex = new SearchVertex();
+                neighborVertex.point = neighbor;
+                neighborVertex.parent = currentVertex;
+                openSet.Add(neighborVertex);
+            }                
+        }
     }
 
 }
